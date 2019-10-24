f4c3ab58dce37186367445a96f69df95997ab90a
otsukayuhi <11628469+otsukayuhi@users.noreply.github.com>
2019-10-24 15:06:28 +0900

    軽くリファクタ

--- pages/docs/[md].js
+++ pages/docs/[md].js
@@ -3,15 +3,16 @@ import marked from 'marked';
 import sanitize from 'sanitize-html';
 import errorpage from '../../markdown/error.md'
 
-const Markdown = ({ md }) => {
+const Docs = ({ md }) => {
   const markdown = md.default || md;
   return <div dangerouslySetInnerHTML={{ __html: sanitize(marked(markdown)) }} />;
 };
 
-Markdown.getInitialProps = async context => {
-  const md = await import(`../../markdown/${context.query.md}.md`)
-    .catch(() => errorpage);
+Docs.getInitialProps = async context => {
+  const md = await import(`../../markdown/${context.query.md}.md`).catch(
+    () => errorpage
+  );
   return { md };
 };
 
-export default Markdown;
+export default Docs;
