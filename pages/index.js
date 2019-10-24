26a514c64b49ddbb89310da4f8e24492a72ec4a3
otsukayuhi <11628469+otsukayuhi@users.noreply.github.com>
2019-10-24 14:09:07 +0900

    エラー処理を追加

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import marked from 'marked'
 import sanitize from 'sanitize-html'
 
 const Index = ({ md }) => {
-  const markdown = sanitize(marked(md.default))
-  return <div dangerouslySetInnerHTML={{ __html: markdown }} />
+  const markdown = md.default || md;
+  return <div dangerouslySetInnerHTML={{ __html: sanitize(marked(markdown)) }} />
 };
 
 Index.getInitialProps = async context => {
   const md = await import(`../markdown/${context.query.md}.md`)
+    .catch(() => '# エラーだお')
   return { md }
 };
 
