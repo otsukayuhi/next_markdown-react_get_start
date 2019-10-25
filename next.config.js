e5cb42187623fa8b6aa655b155c43e1be958f561
otsukayuhi <11628469+otsukayuhi@users.noreply.github.com>
2019-10-25 18:41:30 +0900

    静的書き出しに対応

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.md$/,
-        use: 'raw-loader'
-      }
-    )
+const fs = require("fs");
+const blogPostsFolder = "./markdown";
 
-    return config
+const getPathsForMarkdown = () =>
+  fs.readdirSync(blogPostsFolder).reduce((acc, fileName) => {
+    const trimmedName = fileName.substring(0, fileName.length - 3);
+    return Object.assign(acc, {
+      [`/docs/${trimmedName}`]: {
+        page: "/docs/[md]",
+        query: {
+          md: trimmedName
+        }
+      }
+    });
+  }, {});
+
+module.exports = {
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "frontmatter-markdown-loader"
+    });
+    return config;
+  },
+  async exportPathMap(defaultPathMap) {
+    return {
+      ...defaultPathMap,
+      ...getPathsForMarkdown()
+    };
   }
-}
+};
--- pages/docs/[md].js
+++ pages/docs/[md].js
@@ -1,20 +1,19 @@
 import React from "react";
-import marked from "marked";
 import sanitize from "sanitize-html";
-import errorpage from "../../markdown/error.md";
 
-const Docs = ({ md }) => {
-  const markdown = md.default || md;
-  return (
-    <div dangerouslySetInnerHTML={{ __html: sanitize(marked(markdown)) }} />
-  );
+const Docs = ({ markdown }) => {
+  if (!markdown) return <div>not found</div>;
+  const { html } = markdown.default;
+
+  return <div dangerouslySetInnerHTML={{ __html: sanitize(html) }} />;
 };
 
-Docs.getInitialProps = async context => {
-  const md = await import(`../../markdown/${context.query.md}.md`).catch(
-    () => errorpage
+Docs.getInitialProps = async ({ query }) => {
+  const { md } = query;
+  const markdown = await import(`../../markdown/${md}.md`).catch(
+    () => null
   );
-  return { md };
+  return { markdown };
 };
 
 export default Docs;
