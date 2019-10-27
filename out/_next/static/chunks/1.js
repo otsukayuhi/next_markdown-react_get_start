d0b9d24cad6abd9105de63d4bb8078b0317613f4
otsukayuhi <11628469+otsukayuhi@users.noreply.github.com>
2019-10-27 12:38:56 +0900

    静的ファイル書き出し

---
+++ out/_next/static/chunks/1.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./src/markdown/bar.md":
+/*!*****************************!*\
+  !*** ./src/markdown/bar.md ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("# bar\n\n- サンプルのマークダウンです\n- どうなるかな？？\n\n<script>\n  window.alert('script')\n</script>\n\nhttps://google.com");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=1.js.map
