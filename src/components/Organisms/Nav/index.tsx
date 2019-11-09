9967278e404b79399ae4c16af7e5e4dfd5ead415
otsukayuhi <11628469+otsukayuhi@users.noreply.github.com>
2019-11-09 15:25:36 +0900

    lintの設定を調整

--- src/components/Organisms/Nav/index.tsx
+++ src/components/Organisms/Nav/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
-import { WrapperStyle, HeadingStyle, ListStyle, ItemStyle } from './style'
-import { DataType } from 'gateways/getData'
+import React from 'react';
+import Link from 'next/link';
+import { WrapperStyle, HeadingStyle, ListStyle, ItemStyle } from './style';
+import { DataType } from 'gateways/getData';
 
 type NavProps = {
-  data: DataType
-  currentId: string
-}
+  data: DataType;
+  currentId: string;
+};
 
 const Nav: React.FC<NavProps> = ({ data, currentId }) => {
-  if (!data || !data.post) return null
+  if (!data || !data.post) return null;
 
   const items = data.post.map((item, index) => {
-    const { title, id } = item
-    const itemText = `${index + 1}. ${title}`
+    const { title, id } = item;
+    const itemText = `${index + 1}. ${title}`;
 
     return (
       <ItemStyle key={id}>
@@ -21,15 +21,15 @@ const Nav: React.FC<NavProps> = ({ data, currentId }) => {
           <a>{currentId === id ? <strong>{itemText}</strong> : itemText}</a>
         </Link>
       </ItemStyle>
-    )
-  })
+    );
+  });
 
   return (
     <WrapperStyle>
       <HeadingStyle>Index</HeadingStyle>
       <ListStyle>{items}</ListStyle>
     </WrapperStyle>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/components/Organisms/Nav/style.tsx
+++ src/components/Organisms/Nav/style.tsx
@@ -1,20 +1,20 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const WrapperStyle = styled.nav`
   padding: 50px 20px;
   width: 240px;
   height: 100vh;
   background-color: rgb(247, 247, 247);
-`
+`;
 
 export const HeadingStyle = styled.h2`
   font-size: 20px;
-`
+`;
 
 export const ListStyle = styled.ol`
   margin-top: 15px;
   padding-left: 1em;
-`
+`;
 
 export const ItemStyle = styled.li`
   font-size: 13px;
@@ -31,4 +31,4 @@ export const ItemStyle = styled.li`
       text-decoration: underline;
     }
   }
-`
+`;
